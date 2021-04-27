0b539254aafe71ea073a8fc3e0037af9ca4a7388
chris <christopher.tesoro@kmart.com.au>
2021-04-27 11:40:14 +1000

    push

--- src/find-factors.ts
+++ src/find-factors.ts
@@ -1,11 +1,11 @@
 export const findFactors = (numArr: number[], inputNumber: number) => {
-    const factors = [];
+  const factors = [];
 
   for (let i=0; i < numArr.length; ++i){
 
     if (numArr[i] % inputNumber == 0)
     {
-        factors.push(numArr[i]);
+      factors.push(numArr[i]);
     }
   }
   return factors;
